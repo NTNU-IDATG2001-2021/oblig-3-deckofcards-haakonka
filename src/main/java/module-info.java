1e7cbeb172d0964748c9939ab0f01efc2d8a6629
Haakon Tideman Kanter <haakotka@stud.ntnu.no>
2021-04-16 23:17:21 +0200

    Fixed module-info and added junit dependencies to pom file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,5 +3,7 @@ module no.ntnu.idatg2001 {
     requires javafx.fxml;
 
     opens no.ntnu.idatg2001 to javafx.fxml;
+    opens no.ntnu.idatg2001.cards;
     exports no.ntnu.idatg2001;
+    exports no.ntnu.idatg2001.cards;
 }
